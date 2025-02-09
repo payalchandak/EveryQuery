2b268a5152a2b1123ab04b8bffbf2346c59eb05a
payalchandak <payal.chandak@gmail.com>
2025-02-09 16:18:01 -0500

    evaluate trained model on LVEF 0/1 cohorts

--- src/eval.py
+++ src/eval.py
@@ -14,6 +14,15 @@ from dataset import EveryQueryDataset
 from models.everyquery import EveryQueryModule
 from models.mlp import MLP 
 import ipdb
+import os 
+import torch 
+from tqdm import tqdm 
+import polars as pl 
+from pathlib import Path
+from nested_ragged_tensors.ragged_numpy import JointNestedRaggedTensorDict
+import numpy as np 
+from sklearn.metrics import roc_auc_score, accuracy_score
+import matplotlib.pyplot as plt 
 
 setup_resolvers()
 log = RankedLogger(__name__, rank_zero_only=True)
@@ -26,20 +35,44 @@ def main(cfg: DictConfig) -> None:
     Args:
         cfg (DictConfig): configuration composed by Hydra.
     """
+    pts = set(pl.read_parquet('/storage2/payal/dropbox/private/data/processed/mrn.parquet').filter(pl.col('mgh_mrn').is_not_null()).select(pl.col('empi')).unique().to_numpy().reshape(-1).tolist())
+    pt_lvef = set(pl.read_parquet('/storage2/payal/dropbox/private/data/processed/lvef.parquet').select(pl.col('empi')).unique().to_numpy().reshape(-1).tolist())
+    pt_0 = set(pl.read_parquet('/storage2/payal/dropbox/private/data/processed_lvef_pt/lvef_0.parquet').select(pl.col('empi')).unique().to_numpy().reshape(-1).tolist())
+    pt_1 = set(pl.read_parquet('/storage2/payal/dropbox/private/data/processed_lvef_pt/lvef_1.parquet').select(pl.col('empi')).unique().to_numpy().reshape(-1).tolist())
+    
     cfg.trainer.devices = [0]
-
-    results = {}
-    for code_num in range(9): 
-        cfg.data.test.codes = [f'LVEF_{code_num}']
-        cfg.data.test.values_manual = {f'LVEF_{code_num}':[[0,40]]}
+    for query_code in ['LVEF_0','LVEF_1']: 
+        cfg.data.test.codes = [query_code]
         configure_logging(cfg)
-        metrics, objects = evaluate(cfg)
-        results[code_num] = metrics
-    for x in range(9): 
-        print(x, results[x]['test/occurs_auc'].item())
-    for x in range(9): 
-        print(x, results[x]['test/censor_auc'].item())
-    ipdb.set_trace()
+        metrics, obj = evaluate(cfg)
+        predictions = obj['trainer'].predict(model=obj['model'], dataloaders=obj['datamodule'].test_dataloader(), ckpt_path=cfg.ckpt_path)
+        for cohort_name, cohort in [('LVEF_0',pt_0),('LVEF_1',pt_1)]: 
+            true, pred = [], []
+            for x in predictions: 
+                subj = x['batch']['context']['subject_id'].numpy().tolist()
+                mask = ~x['batch']['answer']['censored'].squeeze(1)
+                idx = torch.Tensor([x in cohort for x in subj])[mask].bool()
+                true.extend(x['occurs_target'].reshape(-1)[idx].long().tolist())
+                pred.extend(torch.sigmoid(x['occurs_logits'].reshape(-1)[idx]).tolist())
+            title = f"Query {query_code} Cohort {cohort_name}"
+            plt.hist(true, bins=25)
+            plt.hist(pred, bins=25)
+            plt.xlim(0,1)
+            plt.title(title)
+            plt.savefig(f"{title}.png")
+            plt.close()
+
+    # results = {}
+    # for code_num in range(9): 
+    #     cfg.data.test.codes = [f'LVEF_{code_num}']
+    #     cfg.data.test.values_manual = {f'LVEF_{code_num}':[[0,40]]}
+    #     configure_logging(cfg)
+    #     metrics, objects = evaluate(cfg)
+    #     results[code_num] = metrics
+    # for x in range(9): 
+    #     print(x, results[x]['test/occurs_auc'].item())
+    # for x in range(9): 
+    #     print(x, results[x]['test/censor_auc'].item())
 
     # eval duration over 1-12 months 
     # cfg.data.test.duration_sampling_strategy = 'fixed'
