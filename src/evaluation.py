c1d0036d94353b582f1960c41eb3b9c2bb73bf44
payalchandak <payal.chandak@gmail.com>
2025-02-17 19:36:36 -0500

    slow registry

--- src/evaluation.py
+++ src/evaluation.py
@@ -8,6 +8,9 @@ from hydra import compose, initialize
 import os 
 import torch 
 from sklearn.metrics import roc_auc_score
+import pickle
+from collections import defaultdict
+from pathlib import Path
 
 def minutes(x):
     if isinstance(x, int): return x
@@ -84,29 +87,86 @@ class Run:
         trainer: Trainer = hydra.utils.instantiate(self.cfg.trainer)
         return trainer
 
-def evaluate(run, query): 
-    cfg = run.cfg
-    cfg.data.test.codes = [query.code]
-    cfg.data.test.duration_sampling_strategy = 'fixed'
-    cfg.data.test.fixed_duration = query.duration
-    cfg.data.test.offset_sampling_strategy = 'fixed'
-    cfg.data.test.fixed_offset = query.offset
-    if query.range is None: 
-        cfg.data.test.default_value_sampling_strategy = 'ignore'
-    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
-    pred = run.trainer.predict(model=run.model, dataloaders=datamodule.test_dataloader())
-    pred = {k: [x[k] for x in pred] for k in pred[0].keys()}  
-    for k in ['censor_target', 'occurs_target']: 
-        pred[k] = torch.cat(pred[k]).reshape(-1).long().tolist()
-    for k in ['censor_logits', 'occurs_logits']: 
-        pred[k] = torch.cat(pred[k]).reshape(-1).long().tolist()
-    return pred 
+class ExperimentRegistry:
+    def __init__(self, registry_path='experiment_registry.pkl'):
+        self.runs = {}
+        self.runs_by_name = defaultdict(list)  
+        self.metrics = defaultdict(dict)  # {(run.dir, query) -> metrics dictionary}
+        self.registry_path = Path(registry_path)
+        self.load()
+
+    def add_run(self, run: Run):
+        self.runs[run.dir] = run
+        self.runs_by_name[run.name].append(run)
+
+    def get_run(self, dir: str):
+        self.runs.get(dir, None)
+
+    def get_runs(self, name: str): 
+        return self.runs_by_name.get(name, [])
+    
+    def store_metrics(self, run: Run, query: Query, metrics: dict):
+        self.metrics[(run.dir, query)] = metrics
+        self.save()
+
+    def get_metrics(self, run: Run, query: Query):
+        return self.metrics.get((run.dir, query))
+    
+    def save(self):
+        with open(self.registry_path, 'wb') as f:
+            pickle.dump({'runs': self.runs, 'runs_by_name': self.runs_by_name, 'metrics': self.metrics}, f)
+    
+    def load(self):
+        if self.registry_path.exists():
+            with open(self.registry_path, 'rb') as f:
+                data = pickle.load(f)
+                self.runs = data.get('runs', {})
+                self.runs_by_name = data.get('runs_by_name', defaultdict(list))
+                self.metrics = data.get('metrics', defaultdict(dict))
+    
+    def predict(self, run, query):
+        cfg = run.cfg
+        cfg.data.test.codes = [query.code]
+        cfg.data.test.duration_sampling_strategy = 'fixed'
+        cfg.data.test.fixed_duration = query.duration
+        cfg.data.test.offset_sampling_strategy = 'fixed'
+        cfg.data.test.fixed_offset = query.offset
+        if query.range is None:
+            cfg.data.test.default_value_sampling_strategy = 'ignore'
+        datamodule: LightningDataModule = hydra.utils.instantiate(cfg.data)
+        pred = run.trainer.predict(model=run.model, dataloaders=datamodule.test_dataloader())
+        assert pred, f"Prediction output is empty for run {run} and query {query}"
+        pred = {k: [x[k] for x in pred] for k in pred[0].keys()}
+        for k in ['censor_target', 'occurs_target']:
+            pred[k] = torch.cat(pred[k]).reshape(-1).long().tolist()
+        for k in ['censor_logits', 'occurs_logits']:
+            data = torch.cat(pred[k]).reshape(-1)
+            pred[k] = data.tolist()
+            pred[k.replace('logits','score')] = torch.sigmoid(data).tolist()
+        return pred
+    
+    def evaluate(self, run, query):
+        stored_metrics = self.get_metrics(run, query)
+        if stored_metrics: 
+            return stored_metrics
+        pred = self.predict(run, query)
+        metrics = {
+            'censor_auc': roc_auc_score(pred['censor_target'], pred['censor_score']),
+            'occurs_auc': roc_auc_score(pred['occurs_target'], pred['occurs_score'])
+        }
+        self.store_metrics(run, query, metrics)
+        return metrics
+
+exp = ExperimentRegistry()
 
 obj = Run('test', '/storage2/payal/EveryQuery/results/2025-02-11_20-24-06_414169')
-pred = evaluate(obj, Query(code='ECG',duration=minutes('5y'), offset=0))
+exp.add_run(obj)
 
-censor_auc = roc_auc_score(pred['censor_target'], pred['censor_logits'])
-occurs_auc = roc_auc_score(pred['occurs_target'], pred['occurs_logits'])
-print(censor_auc, occurs_auc)
+query = Query(code='ECG', duration=minutes('5y'), offset=0)
+metrics = exp.evaluate(obj, query)
+print(metrics)
+
+metrics = exp.evaluate(obj, query)
+print(metrics)
 
 ipdb.set_trace()
