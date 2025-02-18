dacfd9547b76b901d34d1d3cc659cf67abcac898
payalchandak <payal.chandak@gmail.com>
2025-02-17 20:05:03 -0500

    fast registry, dont store run objects

--- src/evaluation.py
+++ src/evaluation.py
@@ -95,41 +95,44 @@ class Run:
 
 class ExperimentRegistry:
     def __init__(self, registry_path='experiment_registry.pkl'):
-        self.runs = {}
-        self.runs_by_name = defaultdict(list)  
-        self.metrics = defaultdict(dict)  # {(run.dir, query) -> metrics dictionary}
+        self.runs_by_dir = {}  # Only names and paths, instantiate on demand
+        self.runs_by_name = defaultdict(set)
+        self.metrics = defaultdict(dict)  # {(dir, query) -> metrics dictionary}
         self.registry_path = Path(registry_path)
         self.load()
 
-    def add_run(self, run: Run):
-        self.runs[run.dir] = run
-        self.runs_by_name[run.name].append(run)
+    def add_run(self, name, dir):
+        self.runs_by_dir[dir] = (name, dir) 
+        self.runs_by_name[name].add(dir)
 
-    def get_run(self, dir: str):
-        self.runs.get(dir, None)
+    def get_run(self, dir):
+        if dir in self.runs_by_dir:
+            name, _ = self.runs_by_dir[dir]
+            return Run(name, dir)
+        return None
 
-    def get_runs(self, name: str): 
-        return self.runs_by_name.get(name, [])
-    
-    def store_metrics(self, run: Run, query: Query, metrics: dict):
-        self.metrics[(run.dir, query)] = metrics
+    def get_runs(self, name):
+        return [self.get_run(dir) for dir in self.runs_by_name.get(name, [])]
+
+    def store_metrics(self, dir, query, metrics):
+        self.metrics[(dir, query)] = metrics
         self.save()
 
-    def get_metrics(self, run: Run, query: Query):
-        return self.metrics.get((run.dir, query))
-    
+    def get_metrics(self, dir, query):
+        return self.metrics.get((dir, query))
+
     def save(self):
         with open(self.registry_path, 'wb') as f:
-            pickle.dump({'runs': self.runs, 'runs_by_name': self.runs_by_name, 'metrics': self.metrics}, f)
-    
+            pickle.dump({'runs_by_dir': self.runs_by_dir, 'runs_by_name': self.runs_by_name, 'metrics': self.metrics}, f)
+
     def load(self):
         if self.registry_path.exists():
             with open(self.registry_path, 'rb') as f:
                 data = pickle.load(f)
-                self.runs = data.get('runs', {})
+                self.runs_by_dir = data.get('runs_by_dir', {})
                 self.runs_by_name = data.get('runs_by_name', defaultdict(list))
                 self.metrics = data.get('metrics', defaultdict(dict))
-    
+
     def predict(self, run, query):
         cfg = run.cfg
         cfg.data.test.codes = [query.code]
@@ -148,31 +151,32 @@ class ExperimentRegistry:
         for k in ['censor_logits', 'occurs_logits']:
             data = torch.cat(pred[k]).reshape(-1)
             pred[k] = data.tolist()
-            pred[k.replace('logits','score')] = torch.sigmoid(data).tolist()
+            pred[k.replace('logits', 'score')] = torch.sigmoid(data).tolist()
         return pred
-    
-    def evaluate(self, run, query):
-        stored_metrics = self.get_metrics(run, query)
-        if stored_metrics: 
+
+    def evaluate(self, dir, query):
+        stored_metrics = self.get_metrics(dir, query)
+        if stored_metrics:
             return stored_metrics
+        run = self.get_run(dir)
         pred = self.predict(run, query)
         metrics = {
             'censor_auc': roc_auc_score(pred['censor_target'], pred['censor_score']),
             'occurs_auc': roc_auc_score(pred['occurs_target'], pred['occurs_score'])
         }
-        self.store_metrics(run, query, metrics)
+        self.store_metrics(dir, query, metrics)
         return metrics
+
 
 exp = ExperimentRegistry()
 
-obj = Run('test', '/storage2/payal/EveryQuery/results/2025-02-11_20-24-06_414169')
-exp.add_run(obj)
+name = 'test'
+dir = '/storage2/payal/EveryQuery/results/2025-02-11_20-24-06_414169'
+exp.add_run(name, dir)
 
 query = Query(code='ECG', duration=minutes('5y'), offset=0)
-metrics = exp.evaluate(obj, query)
+metrics = exp.evaluate(dir, query)
 print(metrics)
 
-metrics = exp.evaluate(obj, query)
+metrics = exp.evaluate(dir, query)
 print(metrics)
-
-ipdb.set_trace()
