a1f13151e98e38f1be0f5c7db236d8c7dd7cfc17
gkondas <gkondas@umich.edu>
2026-04-10 23:56:27 -0400

    added fast config

---
+++ scripts/train_eq_fast.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --job-name=eq_fast
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --mail-user=gbk2114@cumc.columbia.edu
+#SBATCH --mail-type=END,FAIL
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:l40s:1
+#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --time=00:15:00
+
+echo "Allocated GPUs:"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "SLURM_JOB_GPUS=$SLURM_JOB_GPUS"
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+set -euo pipefail
+export PYTHONNOUSERSITE=1
+
+mkdir -p logs
+echo "Starting job on $(hostname) at $(date)"
+echo "SLURM CPU PER TASK: $SLURM_CPUS_PER_TASK"
+
+UVENV="$HOME/eq_stuff/eq"
+
+echo "Using python: $UVENV/bin/python"
+$UVENV/bin/python -c "import sys; print('Executable:', sys.executable)"
+
+cd ~/EveryQuery
+
+set -a
+. ./.env
+set +a
+
+export HYDRA_FULL_ERROR=1
+
+srun $UVENV/bin/python src/every_query/train.py --config-name=fast_config "$@"
+
+echo "Finished at $(date)"
